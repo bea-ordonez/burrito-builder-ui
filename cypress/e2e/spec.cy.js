60c56d25c107b90480a65da58dbdbf379fdc941d
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-04-24 11:43:31 -0600

    Add orders fixture and begin cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import orders from "../fixtures/ordersFixture"
+
+describe('user flows', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/orders', orders)
+    cy.visit('http://localhost:3000/')
   })
-})
+  
+  it('should have the title displayed', () => {
+    cy.get('h1').contains('Burrito Builder')
+  })
+
+  it('should display a form with input/buttons', {
+    
+  })
+
+
+
+} )
+
